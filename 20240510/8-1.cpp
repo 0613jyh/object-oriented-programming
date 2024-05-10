2896031bbcb9e1feeeb191655018070f11f9cb06
0613jyh <0613jyh@naver.com>
2024-05-10 10:49:09 +0900

    p

--- 20240510/8-1.cpp
+++ 20240510/8-1.cpp
@@ -2,7 +2,40 @@
 
 using namespace std;
 
+class Circle {
+    private:
+        int radius;
+    public:
+        Circle() { 
+            radius = 1;
+        }
+        Circle(int radius) {
+            this->radius = radius;
+        }
+        double getArea() {
+            return 3.14 * radius * radius;
+        }
+
+};
+
+void swap(Circle &A, Circle &B) {
+    Circle temp = A;
+    A = B;
+    B = temp;
+}
+
+
+
 int main() {
-    cout << "Hello, world" << endl;
+    Circle A(3), B(5);
+
+    std::cout << "A의 면적 = "  << A.getArea() << " ";
+    std::cout << "B의 면적 = "  << B.getArea() << std::endl;
+
+    swap(A, B);
+
+    std::cout << "A의 면적 = " << A.getArea() << " ";
+    std::cout << "B의 면적 = " << B.getArea() << std::endl;
+
     return 0;
 }
